e83580de46189083acb4685fa6f894fdda63bef2
Shannon V. Crabill <shannon.crabill@gmail.com>
2019-06-13 19:53:43 +0000

    Automatically backed up by Learn

--- lib/my_find.rb
+++ lib/my_find.rb
@@ -5,6 +5,7 @@ def my_find(collection) # The find method returns back the first item in the arr
   while i < collection.length # Keep running while the counter is less than the number of items in the collection array
     if yield(collection[i]) # Pass the current item from the argument into the block, evaluate it, then return the value back
       return collection[i]
+    end
     i = i + 1 # Increment counter by 1
   end
 end
