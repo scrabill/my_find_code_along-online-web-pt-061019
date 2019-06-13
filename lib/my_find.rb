7733f9e9e4ed5a69a8d20db7dc237a0c1dd6a7bd
Shannon V. Crabill <shannon.crabill@gmail.com>
2019-06-13 19:49:40 +0000

    Automatically backed up by Learn

--- lib/my_find.rb
+++ lib/my_find.rb
@@ -2,7 +2,8 @@ require 'pry'
 
 def my_find(collection)
   i = 0 # Counter
-  while i < collection.length #Keep running while the counter is less than the number of items in the collection array
-    i = i + 1 #Increment counter by 1
+  while i < collection.length # Keep running while the counter is less than the number of items in the collection array
+    yield(collection[i]) # Pass
+    i = i + 1 # Increment counter by 1
   end
 end
