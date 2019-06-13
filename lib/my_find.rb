45a71d1f7f58a1fc426543c29172fb8348e39042
Shannon V. Crabill <shannon.crabill@gmail.com>
2019-06-13 19:52:31 +0000

    Automatically backed up by Learn

--- lib/my_find.rb
+++ lib/my_find.rb
@@ -1,6 +1,6 @@
 require 'pry'
 
-def my_find(collection)
+def my_find(collection) # The find method returns back the first item in the array that is true
   i = 0 # Counter
   while i < collection.length # Keep running while the counter is less than the number of items in the collection array
     yield(collection[i]) # Pass the current item from the argument into the block, evaluate it, then return the value back
